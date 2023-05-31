bb0b6b992bd7591dbc0194a27b45a16fa7534024
Chahat Deep Singh <chahat@umd.edu>
2023-05-31 03:23:07 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,13 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_logo = "WorldGenLogo.svg"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': True,
+}
+
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
